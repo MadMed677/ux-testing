6ba59905b204944f410995814f14513dd992625e
Artem Anashev <anashev@yamoney.ru>
2017-05-21 03:50:22 +0300

    Used material-ui

--- src/components/app/app.react.js
+++ src/components/app/app.react.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import ParameterItem from '../parameter-item/parameter-item.react';
+
+import {
+    Step,
+    Stepper,
+    StepLabel,
+    StepContent
+} from 'material-ui/Stepper';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+
+injectTapEventPlugin();
 
 export default class App extends Component {
     state: {
         testName: string,
-        parameters: Array<{ name: string, value: string }>
+        parameters: Array<{ name: string, value: string }>,
+        stepIndex: number,
+        finished: boolean
     } = {
-        parameters: []
+        parameters: [],
+        stepIndex: 0,
+        finished: false
     }
 
     /**
@@ -32,6 +49,47 @@ export default class App extends Component {
                 return item;
             })
         });
+    }
+
+    handleNext = () => {
+        const {stepIndex} = this.state;
+        this.setState({
+            stepIndex: stepIndex + 1,
+            finished: stepIndex >= 2,
+        });
+    };
+
+    handlePrev = () => {
+        const {stepIndex} = this.state;
+        if (stepIndex > 0) {
+            this.setState({stepIndex: stepIndex - 1});
+        }
+    };
+
+    $stepActions = (step: number) => {
+        const { stepIndex } = this.state;
+
+        return (
+            <div style={{margin: '12px 0'}}>
+                { step > 0 && <RaisedButton
+                    label={stepIndex === 2 ? 'Finish' : 'Next'}
+                    disableTouchRipple={true}
+                    disableFocusRipple={true}
+                    primary={true}
+                    onTouchTap={this.handleNext}
+                    style={{marginRight: 12}}
+                />}
+                { step > 0 && (
+                    <RaisedButton
+                        label="Back"
+                        disabled={stepIndex === 0}
+                        disableTouchRipple={true}
+                        disableFocusRipple={true}
+                        onTouchTap={this.handlePrev}
+                    />
+                )}
+            </div>
+        );
     }
 
     /**
@@ -60,6 +118,8 @@ export default class App extends Component {
         this.setState({
             parameters: parameters.map(item => ({ name: item, value: '' }))
         });
+
+        this.handleNext();
         console.log(this.state.parameters);
     }
     
@@ -72,18 +132,46 @@ export default class App extends Component {
 
         return (
             <div className="container">
-                <div className="header"></div>
                 <section className="section">
                     <h1>UX Testing</h1>
 
                     <hr/>
 
-                    <div className="input-group">
-                        <input className="form-control" placeholder="Input Test Name" onChange={ this.onChangeTestName } />
-                        <div className="input-group-btn">
-                            <button className="btn btn-default" onClick={ this.onTestNameSave.bind(this) }>Save</button>
-                        </div>
-                    </div>
+                    <Stepper activeStep={ this.state.stepIndex } orientation="vertical">
+                        <Step>
+                            <StepLabel>Write test name</StepLabel>
+                            <StepContent>
+                                <div className="row">
+                                    <div className="col-sm-6" style={{ 'text-align': 'right' }}>
+                                        <TextField hintText="Input test name" onChange={ this.onChangeTestName } />
+                                    </div>
+                                    <div className="col-sm-6">
+                                        <RaisedButton label="Save" onTouchTap={ this.onTestNameSave.bind(this) } />
+                                    </div>
+                                </div>
+
+                                { this.$stepActions(0) }
+                            </StepContent>
+                        </Step>
+                        <Step>
+                            <StepLabel>Select 2</StepLabel>
+                            <StepContent>
+                                <TextField hintText="Input test name" onChange={ this.onChangeTestName } />
+                                <RaisedButton label="Save" onTouchTap={ this.onTestNameSave.bind(this) } />
+
+                                { this.$stepActions(1) }
+                            </StepContent>
+                        </Step>
+                        <Step>
+                            <StepLabel>Select 3</StepLabel>
+                            <StepContent>
+                                <TextField hintText="Input test name" onChange={ this.onChangeTestName } />
+                                <RaisedButton label="Save" onTouchTap={ this.onTestNameSave.bind(this) } />
+
+                                { this.$stepActions(2) }
+                            </StepContent>
+                        </Step>
+                    </Stepper>
 
                     <div>
                         <hr/>
@@ -91,7 +179,6 @@ export default class App extends Component {
                         { $parameters }
                     </div>
                 </section>
-                <footer className="footer"></footer>
             </div>
         );
     }
